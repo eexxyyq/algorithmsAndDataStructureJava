6f357e0652233bcecd1dca3899938ba7440c2372
Yaroslav TIkhonov <eexxyyq@yandex.ru>
2019-12-24 16:49:58 +0300

    selectionSort

--- src/Sort.java
+++ src/Sort.java
@@ -2,7 +2,8 @@ public class Sort {
     public static void main(String[] args) {
         Sort sort = new Sort();
         int[] array = new int[]{1,5,7,4,3,8,0,6,2,9};
-        sort.bubbleSort(array);
+        //sort.bubbleSort(array);
+        sort.selectionSort(array );
         for (int x : array) {
             System.out.println(x);
         }
@@ -10,7 +11,6 @@ public class Sort {
 
     /**
      * Bubble Sort realization
-     *
      */
     public void bubbleSort(int[] array) {
         for (int i = array.length - 1; i >= 1; i--) {
@@ -20,6 +20,27 @@ public class Sort {
                     array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
+            }
+        }
+    }
+
+    /**
+     *Selection sort realization
+     */
+    public void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    index= j;
+                }
+            }
+            if (i != index) {
+                int tmp = array[i];
+                array[i] = array[index];
+                array[index] = tmp;
             }
         }
     }
