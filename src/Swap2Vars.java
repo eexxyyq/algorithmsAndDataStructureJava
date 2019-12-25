cdf76d020582131744822affb96ec2c634f094eb
Yaroslav Tikhonov <eexxyyq@yandex.ru>
2019-12-26 02:05:59 +0300

    swap 2 variables fixSmth

--- src/Swap2Vars.java
+++ src/Swap2Vars.java
@@ -1,16 +1,18 @@
 public class Swap2Vars {
     static void swapXOR(int a, int b) {
+        System.out.println("Was: " + a + " " + b);
         a = a ^ b;
         b = b ^ a;
         a = a ^ b;
-        System.out.println(a + " " + b);
+        System.out.println("Now: " + a + " " + b);
     }
 
     static void swapArithmetic(int a, int b) {
+        System.out.println("Was: " + a + " " + b);
         a = a + b;
         b = - b + a;
         a = a - b;
-        System.out.println(a + " " + b);
+        System.out.println("Now: " + a + " " + b);
     }
     public static void main(String[] args) {
         int a = 45;
